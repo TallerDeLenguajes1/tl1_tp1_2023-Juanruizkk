1824fedefbfd9889ede111237b7923c8e9a5425c
juanruizkk <juanignacioruizr@gmail.com>
2023-03-21 21:55:00 -0300

    segundo commit

---
+++ tp1_1.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+int main(){
+    int x = 10;
+    int *px;
+    px = &x;
+    printf("contendio de la vrble con el puntero:%d\n", *px);
+    printf("direccion de memoria del puntero%p\n", px);
+    printf("direccion de memoria de la vrble:%d\n", &x);
+    printf("direccion memoria del puntero:%d\n", &px);
+    printf("tamanio de la memoria:%d\n", sizeof(int));
+
+
+}
