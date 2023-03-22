e236c0a8e6fc654d4b1d5ce7adc5507fb6b81efc
juanruizkk <juanignacioruizr@gmail.com>
2023-03-21 22:32:28 -0300

    Ejercicio 2.4 completado

--- tp1_1.c
+++ tp1_1.c
@@ -4,7 +4,7 @@ int main(){
     int *px;
     px = &x;
     printf("contendio de la vrble con el puntero:%d\n", *px);
-    printf("direccion de memoria del puntero%p\n", px);
+    printf("direccion de memoria del puntero%d\n", &(*px));
     printf("direccion de memoria de la vrble:%d\n", &x);
     printf("direccion memoria del puntero:%d\n", &px);
     printf("tamanio de la memoria:%d\n", sizeof(int));
